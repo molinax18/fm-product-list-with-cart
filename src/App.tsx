1822c9eaa8c7b78c526ed9c40d60edbd51762dad
molinax18 <arielmolina8953@gmail.com>
2026-02-20 11:47:37 -0300

    feat(components): new products chore(styles): css layer base chore(products): products image src path

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import Cart from "./components/Cart";
+import Products from "./components/Products";
+
 export default function App() {
-  return <div className="text-red-500">App</div>;
+  return (
+    <main className="grid grid-cols-1 justify-center gap-4 container mx-auto p-4 md:grid-cols-[65%_35%]">
+      <Products />
+      <Cart />
+    </main>
+  );
 }
---
+++ src/components/Product.tsx
@@ -0,0 +1,25 @@
+import type { ProductType } from "@/types/product";
+
+export default function Product({ product }: { product: ProductType }) {
+  return (
+    <article className="flex flex-col bg-white rounded-lg shadow-md overflow-hidden max-w-96">
+      <picture>
+        <source media="(min-width:650px)" srcSet={product.image.desktop} />
+        <source media="(min-width:465px)" srcSet={product.image.tablet} />
+        <img
+          src={product.image.mobile}
+          alt={product.name}
+          className="rounded-lg object-contain"
+        />
+      </picture>
+      <button className="self-center -mt-5 py-1 px-3 rounded-full bg-white border border-red cursor-pointer">
+        Add to Cart
+      </button>
+      <section className="flex flex-col gap-y-2 p-4">
+        <p className="text-rose-300">{product.category}</p>
+        <h3 className="font-semibold">{product.name}</h3>
+        <p className="font-semibold text-red">${product.price}</p>
+      </section>
+    </article>
+  );
+}
